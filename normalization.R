973904ae3137c0de905e57239b5b143aa7a4d7a9
Yunzhang Wang <yunwan@kth.se>
2015-03-11 13:55:40 +0100

    include TPM normalization

--- normalization.R
+++ normalization.R
@@ -1,30 +1,25 @@
 setwd("/home/yunzhang/CAGE_project/out/counts/")
 
 # Load data
-yeast = read.table(file = "CAGE_VRSY_HELA_SPIKE_01P_1P_r64_counts.bed",stringsAsFactors=FALSE)
-human = read.table(file = "CAGE_VRSY_HELA_SPIKE_01P_1P_hg19_counts.bed",stringsAsFactors=FALSE)
-colnames(yeast) = c("Tag","CAGE01","CAGE1")
-colnames(human) = c("Tag","CAGE01","CAGE1")
+yeast = read.table(file = "CAGE_VRSY_HELA_SPIKE_01P_1P_r64_counts.bed",stringsAsFactors=FALSE,row.names=1)
+human = read.table(file = "CAGE_VRSY_HELA_SPIKE_01P_1P_hg19_counts.bed",stringsAsFactors=FALSE,row.names=1)
+colnames(yeast) = c("CAGE01","CAGE1")
+colnames(human) = c("CAGE01","CAGE1")
 
-summary(human)
+# TPM normalization
+result_human <- apply(human,2,function(x){
+	y <- x*10^6/sum(x)
+	return(y)
+})
+result_human <- as.data.frame(result_human)
+write.table(result_human,"normalized_TPM_human.bed",sep = "\t",col.names = NA)
 
-       Tag                CAGE01             CAGE1
- Length:37925       Min.   :     0.0   Min.   :     0.0
- Class :character   1st Qu.:    29.0   1st Qu.:    31.0
- Mode  :character   Median :    71.0   Median :    69.0
-                    Mean   :   577.1   Mean   :   546.2
-                    3rd Qu.:   283.0   3rd Qu.:   266.0
-                    Max.   :260101.0   Max.   :290616.0
-
-
-summary(yeast)
-     Tag                CAGE01            CAGE1
- Length:1089        Min.   :   0.00   Min.   :   1.0
- Class :character   1st Qu.:   4.00   1st Qu.:  34.0
- Mode  :character   Median :   7.00   Median :  55.0
-                    Mean   :  45.51   Mean   : 154.8
-                    3rd Qu.:  17.00   3rd Qu.: 110.0
-                    Max.   :5559.00   Max.   :8226.0
+result_yeast <- apply(yeast,2,function(x){
+	y <- x*10^6/sum(x)
+	return(y)
+})
+result_yeast <- as.data.frame(result_yeast)
+write.table(result_yeast,"normalized_TPM_yeast.bed",sep = "\t",col.names = NA)
 
 
 # normalization
@@ -49,5 +44,5 @@ normalized_human$norm_1 <- 10^h1
 return(normalized_human)
 }
 
-result <- CAGE_normalization(yeast,human)
+result <- CAGE_normalization(result_yeast,result_human)
 write.table(result,"normalized_human.bed")
