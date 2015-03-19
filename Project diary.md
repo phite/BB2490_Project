ce14778df1e3d07c99466937ca26a4caa743eaf8
Yunzhang Wang <yunwan@kth.se>
2015-03-19 11:06:05 +0100

    update diary and presentation files

--- Project diary.md
+++ Project diary.md
@@ -3,10 +3,16 @@ Project Start
 
 Discuss project topic, plan and diaries with Enrichetta and Macro
 
+Project idea [presentation](https://github.com/phite/BB2490_Project/blob/master/cage_presentation.pdf)
+
 ###Project plan
 -  **1st week:**  go through the CAGE data pipeline
 
--  **2nd week:**  start data analysis
+-  **2nd week:**  build up a blacklist
+
+-  **3nd week:**  data normalization and analysis
+
+-  **4nd week:**  prepare for the poster session
 
 -----------------------------
 
@@ -21,15 +27,15 @@ Read through data processing pipeline from Enrichetta
 
 Get more detailed information about data processing part of pipeline.
 
-An additional step: map reads to a black list which contains identical sequence of human and yeast transcriptome.
+An additional step: map reads to a black list which contains identical sequence of human and yeast genome.
 
-Get raw data shared from Macro
+Get raw data shared from Macro.
 
 ------------------------------
 
 ##2015-02-25 Wed
 
-Read and change [script](https://github.com/phite/BB2490_Project/blob/master/pipeline/SCR_MM_mapping_01.sh) written by Enrichetta
+Read, edit and correct errors in [script](https://github.com/phite/BB2490_Project/blob/master/pipeline/SCR_MM_mapping_01.sh) written by Enrichetta.
 
 Practice the Data processing of CAGE_01yeast data using shell commands.
 
@@ -37,11 +43,11 @@ Practice the Data processing of CAGE_01yeast data using shell commands.
 
 ##2015-02-27 Fri
 
-Meeting with Enrichetta and Marco.
+Meeting with Enrichetta and Marco in SciLifeLab.
 
-Discuss questions of running script and the problem of creating blacklist.
+Discuss problems of running script and creating blacklist.
 
-Receive codes [chop](https://github.com/phite/BB2490_Project/blob/master/pipeline/chop_seq.py) and [merge](https://github.com/phite/BB2490_Project/blob/master/pipeline/merge.py) from Marco
+Receive codes [chop](https://github.com/phite/BB2490_Project/blob/master/pipeline/chop_seq.py) and [merge](https://github.com/phite/BB2490_Project/blob/master/pipeline/merge.py) from Marco, which are used for creating blacklist.
 
 -------------------------------
 
@@ -57,7 +63,7 @@ Improve and complete the [pipeline](https://github.com/phite/BB2490_Project/blob
 
 ##2015-03-03 Tue
 
-Describe problems and exchange experiences in seminar.
+Describe problems and exchange experiences in seminar. [Presentation](https://github.com/phite/BB2490_Project/blob/master/CAGE_presentation_3.3.pdf)
 
 Run preprocessing pipeline for both samples ([script_01P](https://github.com/phite/BB2490_Project/blob/master/pipeline/SCR_MM_mapping_01.sh) and [script_1P](https://github.com/phite/BB2490_Project/blob/master/pipeline/SCR_MM_mapping_1.sh)) using two blacklists.
 
@@ -77,9 +83,9 @@ Receive codes for step 2-4 in the [pipeline](https://github.com/phite/BB2490_Pro
 
 ##2015-03-06 Fri
 
-Meeting with Enrichetta and Marco.
+Meeting with Enrichetta and Marco in SciLifeLab.
 
-We discussed the small results difference after running preprocessing.
+We discussed the small difference in results after running preprocessing.
 
 Also, we try to figure out a method to normalize human read counts according to the dynamic range of yeast read counts.
 
@@ -87,9 +93,9 @@ Also, we try to figure out a method to normalize human read counts according to 
 
 ##2015-03-09 Mon
 
-Think about a normalization method using mean of logarithm of yeast read counts to adjust human counts.
+Think about a [normalization method](https://github.com/phite/BB2490_Project/blob/master/read_count/Normalization_Yunzhang.docx) using mean of logarithm of yeast read counts to adjust human counts.
 
-Seminar about how to prepare poster and poster presentation.
+Seminar about how to prepare the poster and the poster session.
 
 Discuss normalization methods with Olof and receive and related [article](http://www.nature.com/nbt/journal/v32/n9/full/nbt.2931.html) from him.
 
@@ -110,9 +116,52 @@ Perform a quick analysis and try to normalize read counts in R:
 
 ##2015-03-11 Wed
 
-Receive codes for TPM normalization from Enrichetta. Add them in [normalization.R](https://github.com/phite/BB2490_Project/blob/master/read_count/normalization.R).
+Receive codes for TPM normalization from Enrichetta. Rearrange and add them in [normalization.R](https://github.com/phite/BB2490_Project/blob/master/read_count/normalization.R).
+
+-------------------------------
+
+##2015-03-12 Thu
+
+Try R package [TCC](http://master.bioconductor.org/packages/release/bioc/html/TCC.html) to analyse read count.
+
+Try various methods to normalize data.
+
+Use boxplot of Relative Log Expression (RLE) from R package [AgiMicroRna](http://www.bioconductor.org/packages/release/bioc/html/AgiMicroRna.html) to evaluate different normalization methods.
 
 
+-------------------------------
+
+##2015-03-13 Fri
+
+Study the normalization method used by Marco from R package [DESeq](http://master.bioconductor.org/packages/release/bioc/html/DESeq.html)
+
+-------------------------------
+
+##2015-03-15 Sun
+
+Make a draft of poster.
+
+-------------------------------
+
+##2015-03-17 Tue
+
+Continue working on poster.
+
+Meeting with Enrichetta and Macro in Karolinska Huddinge.
+
+Discuss normalization methods, poster structure and layout.
+
+-------------------------------
+
+##2015-03-18 Wed
+
+Finish evaluating the three methods we used to normalize read count and create boxplots.
+
+Meeting with Enrichetta and Macro in SciLifeLab.
+
+Discuss last details in poster.
+
+Complete final version of the [poster](https://github.com/phite/BB2490_Project/blob/master/CAGE-data-analysis-of-HeLa-yeast-spike-in.pdf)
 
 
 
--- read_count/normalization.R
+++ read_count/normalization.R
@@ -1,10 +1,43 @@
-setwd("~/CAGE_project/out/counts/")
+setwd("~/Dropbox/CAGE_project/final_counts/")
+require(AgiMicroRna)
+require(DESeq)
 
 # Load data
-yeast = read.table(file = "CAGE_VRSY_HELA_SPIKE_01P_1P_r64_counts.bed",stringsAsFactors=FALSE,row.names=1)
-human = read.table(file = "CAGE_VRSY_HELA_SPIKE_01P_1P_hg19_counts.bed",stringsAsFactors=FALSE,row.names=1)
-colnames(yeast) = c("CAGE01","CAGE1")
-colnames(human) = c("CAGE01","CAGE1")
+yeast <- read.table(file = "CAGE_VRSY_HELA_SPIKE_01P_1P_r64_counts.bed",stringsAsFactors=FALSE,row.names=1)
+human <- read.table(file = "CAGE_VRSY_HELA_SPIKE_01P_1P_hg19_counts.bed",stringsAsFactors=FALSE,row.names=1)
+
+RLE_human <- read.table(file = "outputFile_human_RLE_normalized.txt",stringsAsFactors=FALSE,row.names="ids")
+RLE_yeast <- read.table(file = "outputFile_yeast_RLE_normalized.txt",stringsAsFactors=FALSE,row.names="ids")
+RLE_human$row.names = RLE_yeast$row.names = NULL
+
+spikein <- read.table(file = "outputFile_SpikeIn_Normalized.txt",stringsAsFactors=FALSE,row.names="ids")
+spikein$row.names <- NULL
+spikein$CAGE1 <- spikein$CAGE1*10
+colnames(spikein) <- c("spikein_01","spikein_1")
+
+conds <- c("CAGE01","CAGE1")
+colnames(yeast) <- conds
+colnames(human) <- conds
+
+# DESeq
+
+yeast_cds <- newCountDataSet(yeast,conds)
+yeast_cds <- estimateSizeFactors(yeast_cds)
+
+human_cds <- newCountDataSet(human,conds)
+human_cds <- estimateSizeFactors(human_cds)
+
+sf =  sizeFactors(yeast_cds)
+sf[1]= sf[1]*10
+sizeFactors(human_cds) <- sf
+foo = counts(human_cds, normalized=T)
+ foo = as.data.frame(foo,stringsAsFactors = F)
+
+
+human_cds <- estimateSizeFactors(human_cds)
+human_cds <- estimateDispersions( human_cds, method = "blind",sharingMode="fit-only") # WE MADE THIS CHANGE BECAUSE WE DO NOT HAVE OUTLIERS 
+res <- nbinomTest( human_cds, "CAGE01", "CAGE1" )
+
 
 # TPM normalization
 
@@ -17,10 +50,47 @@ TPM_normalization <- function(dat){
 return(result_data)
 }
 
-result_human <- TPM_normalization(human)
-write.table(result_human,"normalized_TPM_human.bed",sep = "\t",col.names = NA)
-result_yeast <- TPM_normalization(yeast)
-write.table(result_yeast,"normalized_TPM_yeast.bed",sep = "\t",col.names = NA)
+TPM_human <- TPM_normalization(human)
+write.table(TPM_human,"normalized_TPM_human.bed",sep = "\t",col.names = NA)
+TPM_yeast <- TPM_normalization(yeast)
+write.table(TPM_yeast,"normalized_TPM_yeast.bed",sep = "\t",col.names = NA)
+
+# remove outlayer
+
+rm_out <- function(dat,num) {
+	rmlist <- numeric()
+	for(i in 1: nrow(dat)){
+		if(any(dat[i,] > num))
+		rmlist <- c(rmlist,i)
+	}
+	rm_dat <- dat[-rmlist,]
+	
+return(rm_dat)
+}
+
+# remove -Inf
+rm_inf <- function(dat) {
+	rmlist <- numeric()
+	for(i in 1: nrow(dat)){
+		if(any(dat[i,] == "-Inf"))
+		rmlist <- c(rmlist,i)
+	}
+	rm_dat <- dat[-rmlist,]
+	
+return(rm_dat)
+}
+
+# foo <- cbind(human,res$baseMeanA,res$baseMeanB)
+
+# foo <- rm_inf(log2(rm_out(foo,1000)))
+# RleMicroRna(foo)
+
+
+# foo <- rm_inf(log2(rm_out(human,1000)))
+# hist(foo$CAGE01,breaks = seq(from = 0,to = 10,by = 0.1))
+
+# plot(density(foo$CAGE01)) # returns the density data 
+# plot(d)
 
 # normalization
 CAGE_normalization <- function(yeast,human){
@@ -44,5 +114,160 @@ normalized_human$norm_1 <- 10^h1
 return(normalized_human)
 }
 
-result <- CAGE_normalization(result_yeast,result_human)
+YZ_human <- CAGE_normalization(yeast,human)
+YZ_human$CAGE01 = YZ_human$CAGE1 = NULL
+
 write.table(result,"normalized_human.bed",col.names = NA)
+
+
+colnames(TPM_human) <- c("TPM_01","TPM_1")
+colnames(RLE_human) <- c("RLE_01","RLE_1")
+
+# plot no yeast normalization
+pdf("no_yeast.pdf")
+par(mfrow=c(1,3))
+foo <- rm_inf(log2(human))
+RleMicroRna(foo,colorfill="blue")
+
+foo <- rm_inf(log2(TPM_human))
+RleMicroRna(foo,colorfill="orange")
+
+foo <- rm_inf(log2(RLE_human))
+RleMicroRna(foo,colorfill="green")
+dev.off()
+
+
+# plot normalization with yeast
+pdf("with_yeast.pdf")
+par(mfrow=c(1,3))
+foo <- rm_inf(log2(human))
+RleMicroRna(foo,colorfill="blue")
+
+foo <- rm_inf(log2(spikein))
+RleMicroRna(foo,colorfill="orange")
+
+foo <- rm_inf(log2(YZ_human))
+RleMicroRna(foo,colorfill="green")
+dev.off()
+
+
+
+##############################################################
+
+
+> t.test(human[,1],human[,2])
+
+        Welch Two Sample t-test
+
+data:  human[, 1] and human[, 2]
+t = 1.2257, df = 75785.91, p-value = 0.2203
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -18.49834  80.26018
+sample estimates:
+mean of x mean of y
+ 577.1233  546.2423
+
+> t.test(TPM_human[,1],TPM_human[,2])
+
+        Welch Two Sample t-test
+
+data:  TPM_human[, 1] and TPM_human[, 2]
+t = 0, df = 75323.7, p-value = 1
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -2.322531  2.322531
+sample estimates:
+mean of x mean of y
+ 26.36783  26.36783
+
+> t.test(RLE_human[,1],RLE_human[,2])
+
+        Welch Two Sample t-test
+
+data:  RLE_human[, 1] and RLE_human[, 2]
+t = 0.7009, df = 75642.56, p-value = 0.4834
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -31.73664  67.06880
+sample estimates:
+mean of x mean of y
+ 570.3731  552.7070
+
+
+> t.test(spikein[,1],spikein[,2])
+
+        Welch Two Sample t-test
+
+data:  spikein[, 1] and spikein[, 2]
+t = -2.809, df = 72706.12, p-value = 0.004971
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -384.08035  -68.37489
+sample estimates:
+mean of x mean of y
+ 1666.011  1892.239
+
+t.test(YZ_human[,1],YZ_human[,2])
+
+        Welch Two Sample t-test
+
+data:  YZ_human[, 1] and YZ_human[, 2]
+t = -5.3087, df = 68949.69, p-value = 1.107e-07
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -236.7340 -109.0638
+sample estimates:
+mean of x mean of y
+ 625.9221  798.8210
+
+#################################################
+
+log2human <- rm_inf(log2(human))
+t.test(log2human[,1],log2human[,2])
+
+        Welch Two Sample t-test
+
+data:  log2human[, 1] and log2human[, 2]
+t = -0.5506, df = 75435.01, p-value = 0.5819
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -0.04275003  0.02399773
+sample estimates:
+mean of x mean of y
+ 6.640545  6.649921
+
+log2humanTPM <- rm_inf(log2(TPM_human))
+t.test(log2humanTPM[,1],log2humanTPM[,2])
+
+        Welch Two Sample t-test
+
+data:  log2humanTPM[, 1] and log2humanTPM[, 2]
+t = -5.2101, df = 75435.01, p-value = 1.893e-07
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -0.12208837 -0.05534061
+sample estimates:
+mean of x mean of y
+ 2.188516  2.277231
+
+log2humanRLE <- rm_inf(log2(RLE_human))
+t.test(log2humanRLE[,1],log2humanRLE[,2])
+
+
+        Welch Two Sample t-test
+
+data:  log2humanRLE[, 1] and log2humanRLE[, 2]
+t = -2.5443, df = 75435.01, p-value = 0.01095
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -0.076697367 -0.009949602
+sample estimates:
+mean of x mean of y
+ 6.623571  6.666895
+
+log2spikein <- rm_inf(log2(spikein))
+ t.test(log2spikein[,1],log2spikein[,2])
+
+
+
